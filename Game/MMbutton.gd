9f9ab2e1ba5501793ef2be4f2354f252e6e8d7cf
Lily Swift <lilyswift246@gmail.com>
2025-08-22 21:06:14 -0700

    Added basic main menu

---
+++ Game/MMbutton.gd
@@ -0,0 +1,29 @@
+class_name MainMenu extends Node
+
+@export var startButton : Button;
+@export var optionsButton : Button;
+@export var creditsButton : Button;
+@export var exitButton : Button;
+
+var levelScene : PackedScene;
+
+func _ready():
+	levelScene = preload("res://level.tscn");
+	startButton.pressed.connect(_start_pressed);
+	optionsButton.pressed.connect(_options_pressed);
+	creditsButton.pressed.connect(_credits_pressed);
+	exitButton.pressed.connect(_exit_pressed);
+	
+
+
+func _start_pressed():
+	get_tree().change_scene_to_packed(levelScene);
+	
+func _options_pressed():
+	pass
+
+func _credits_pressed():
+	pass
+
+func _exit_pressed():
+	pass
