6c3832bafe0d2ad9041da29bae69be45fc24407e
Lily Swift <lilyswift246@gmail.com>
2025-08-23 13:51:34 -0700

    Created character physics and stuff

--- Game/playerController.gd
+++ Game/playerController.gd
@@ -1,6 +1,8 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 var input_directionW : int = 0
+
+var rot : float = 0;
 
 func _ready():
 	$AnimatedSprite2D.play("Idle");
@@ -11,3 +13,10 @@ func get_input():
 		input_directionW -= 1
 	if Input.is_action_pressed("right"):
 		input_directionW += 1
+
+
+func _process(delta):
+	rot += 1*delta;
+	if rot >= 360:
+		rot -= 360;
+	rotation = rot;
