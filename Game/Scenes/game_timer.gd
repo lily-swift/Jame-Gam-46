2e7ecc71facd7151ac46c9cee10a68ccdad6b8cb
DimirAgentAshley <52061722+DimirAgentAshley@users.noreply.github.com>
2025-08-24 11:13:23 -0700

    timer script

---
+++ Game/Scenes/game_timer.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@onready var label = $Label
+@onready var timer = $Timer
+
+var endScene : PackedScene;
+
+func _ready():
+	endScene = preload("res://Scenes/main_menu.tscn");
+	timer.start()
+	
+func time_left():
+	var time_left = timer.time_left
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	return [minute, second]
+	
+func _process(delta):
+	label.text = "%02d:%02d" % time_left()
+	if time_left() == [0.0,0]:
+		get_tree().change_scene_to_packed(endScene);
