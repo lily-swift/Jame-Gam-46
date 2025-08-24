8b0999ebca80fecc3a3af89c01f84112068d8244
Lily Swift <lilyswift246@gmail.com>
2025-08-24 14:15:13 -0700

    askdn as

---
+++ Game/Scenes/camera_2d.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+var positionTracker : Vector2
+@export var parent : Node2D
+
+func _init():
+	positionTracker = position 
+
+func _process(delta):
+	var tx = parent.position.x
+	var ty = parent.position.y
+	var x = positionTracker.x
+	var y = positionTracker.y
+	if(x > tx + 75):
+		positionTracker.x = tx+75
+	if(x < tx - 75):
+		positionTracker.x = tx-75
+	if(y > ty + 60):
+		positionTracker.y = ty+60
+	if(y < ty+5  and not (ty+5 > -10)):
+		positionTracker.y = ty+5
+		
+	if(y>-10):
+		positionTracker.y = -10
+	
+	position = Vector2(round(positionTracker.x),round(positionTracker.y))
