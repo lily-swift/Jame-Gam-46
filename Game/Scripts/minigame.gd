b0e6891dac1bb244dcec5d0f026c043df19a99e5
Vivian A. <vallen813@gmail.com>
2025-08-24 22:48:38 -0700

    minigame works!

--- Game/Scripts/minigame.gd
+++ Game/Scripts/minigame.gd
@@ -1,13 +1,17 @@
 extends Node2D
-@export var startTimer : float
+
+@export var startDelay : float
 @export var cursorSpeed : float
 @export var winBarCount : int
 @export var winBarPixelHeight : int
 
 var cursorCollisionShape : CollisionShape2D
 var cursorRigidBody2D : RigidBody2D
+var initCursorPosition : Vector2
 var minigameFinishArea2D : Area2D
 var winBarDict : Dictionary
+var isActive : bool = false
+var startTimer : float
 
 var winBarRef = preload("res://Scenes/minigame_win_bar.tscn")
 
@@ -16,7 +20,32 @@ func _ready() -> void:
 	cursorCollisionShape = $Minigame_Cursor/CollisionShape2D
 	cursorRigidBody2D = $Minigame_Cursor
 	minigameFinishArea2D = $Minigame_Bar/Area2D
+	initCursorPosition = cursorRigidBody2D.position
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not isActive:
+		#print("Cursor velocity", cursorRigidBody2D.linear_velocity)
+		return
+		
+	if startTimer > 0:
+		startTimer -= delta
+		return
+		
+	if cursorCollisionShape.global_position.y >= minigameFinishArea2D.global_position.y:
+		if winBarDict.size() == 0:
+			print("Minigame win!")
+		print("Minigame finished!")
+		Reset()
+		Hide()
+		return
+	cursorRigidBody2D.linear_velocity = Vector2(0, cursorSpeed)
+
+func _clicked(winBar: Sprite2D) -> void:
+	winBarDict.erase(winBar)
+
+func Start() -> void:
+	startTimer = startDelay
 	for i in range(winBarCount):
 		var bar : Sprite2D = winBarRef.instantiate()
 		add_child(bar)
@@ -29,24 +58,27 @@ func _ready() -> void:
 		bar.position = newPosition
 		
 		winBarDict.get_or_add(bar, newPosition)
+		isActive = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if startTimer > 0:
-		startTimer -= delta
-		return
-	if cursorCollisionShape.global_position.y >= minigameFinishArea2D.global_position.y:
-		if winBarCount <= 0:
-			print("Minigame win!")
-		print("Minigame finished!")
-		queue_free()
-	cursorRigidBody2D.linear_velocity = Vector2(0, cursorSpeed)
+func Reset() -> void:
+	isActive = false
+	cursorRigidBody2D.linear_velocity = Vector2(0,0)
+	cursorRigidBody2D.position = initCursorPosition
+	for bar in winBarDict.keys():
+		bar.queue_free()
+	winBarDict.clear()
 
-func _clicked() -> void:
-	winBarCount -= 1
+func Hide() -> void:
+	pass
 
 func IsOverlap(pos : Vector2) -> bool:
 	for barPos in winBarDict.values():
 		if pos.distance_to(barPos) <= winBarPixelHeight + 1:
 			return true
 	return false
+
+## For debugging purposes
+func _input(event) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_L:
+			Start()
--- Game/Scripts/win_bar.gd
+++ Game/Scripts/win_bar.gd
@@ -1,12 +1,12 @@
 extends Sprite2D
 
 var cursorOverlap : bool = false
-signal clicked
+signal clicked(winBar : Sprite2D)
 
 func _input(event) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_SPACE and cursorOverlap:
-			clicked.emit()
+			clicked.emit(self)
 			queue_free()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
